4e873f721073659000ab0921f331cddee824fdac
unknown <merajssm786@gmail.com>
2023-03-29 17:10:18 +0530

    [import fix]

--- src/pages/Coupan.jsx
+++ src/pages/Coupan.jsx
@@ -4,10 +4,7 @@ import { useEffect } from "react";
 import { useAppStore } from "../appStore";
 import PageTemplate from "./reusable/PageTemplate";
 import {
-  Modal,
   Stack,
-  Autocomplete,
-  TextField,
   Typography,
   Button,
 } from "@mui/material";
@@ -16,18 +13,6 @@ import { db } from "../firebase-config";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 export default function Coupans() {
-  const rows = useAppStore((state) => state.rows);
-  const setRows = useAppStore((state) => state.setRows);
-  const menuRef = collection(db, "Menu");
-
-  useEffect(() => {
-    getMenuData();
-  }, []);
-
-  const getMenuData = async () => {
-    const data = await getDocs(menuRef);
-    setRows(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  };
 
   return (
     <>
@@ -35,16 +20,6 @@ export default function Coupans() {
         title="Coupan List"
         actionBar={
           <Stack direction="row" spacing={2} className="my-2 mb-2">
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={rows}
-              sx={{ width: 300 }}
-              getOptionLabel={(rows) => rows.name}
-              renderInput={(params) => (
-                <TextField {...params} size="small" label="Search Coupan" />
-              )}
-            />
             <Typography
               variant="h6"
               component="div"
