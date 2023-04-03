7b202c500408aedb87ec1f37cdb49036ba216489
unknown <merajssm786@gmail.com>
2023-04-03 20:17:33 +0530

    [code change for Add Promo Code button]

--- src/promo_codes/components/PromoCodeList.js
+++ src/promo_codes/components/PromoCodeList.js
@@ -11,7 +11,7 @@ import {
   IconButton,
 } from "@mui/material";
 import { db } from "../../firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
@@ -29,79 +29,77 @@ function PromoCodeList() {
     const data = await getDocs(ref);
     setPromoCodeData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
-  console.log(promocodeData);
-  return (
-    <>
-      <TableContainer>
-        <Table aria-label="sticky table" stickyHeader>
-          <TableHead>
-            <TableRow>
-              <TableCell align="left" style={{ minWidth: "100px" }}>
-                Title
-              </TableCell>
-              <TableCell align="left" style={{ minWidth: "100px" }}>
-                Start Date
-              </TableCell>
-              <TableCell align="left" style={{ minWidth: "100px" }}>
-                End Date
-              </TableCell>
-              <TableCell align="left" style={{ minWidth: "100px" }}>
-                Promo Code Dis.(%)
-              </TableCell>
-              <TableCell align="left" style={{ minWidth: "100px" }}>
-                Is Offer Live
-              </TableCell>
-              <TableCell align="left" style={{ width: "100px" }}>
-                Action
-              </TableCell>
-              {/* <TableCell align="left" style={{ width: "100px" }}>
-                View Products
-              </TableCell> */}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {promocodeData.map((row) => (
-              <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                <TableCell align="left">{String(row.title)}</TableCell>
-                <TableCell align="left">{String(row.startDate)}</TableCell>
-                <TableCell align="left">{String(row.endDate)}</TableCell>
-                <TableCell align="left">
-                  {String(row.discountPercent)}
-                </TableCell>
-                <TableCell align="left">
-                  {String(row.isOfferLive ? "Yes" : "No")}
-                </TableCell>
-                <TableCell align="left">
-                  <Stack spacing={2} direction="row">
-                    <EditIcon
-                      style={{
-                        fontSize: "20px",
-                        color: "blue",
-                        cursor: "pointer",
-                      }}
-                      className="cursor-pointer"
-                    />
-                    <DeleteIcon
-                      style={{
-                        fontSize: "20px",
-                        color: "darkred",
-                        cursor: "pointer",
-                      }}
-                    />
-                  </Stack>
-                </TableCell>
-                {/* <TableCell align="center">
-                  <Link to={`/offer-details/${row.id}`} target="_blank" rel="noopener noreferrer">
-                      <OpenInNewIcon color="primary"/>
-                  </Link>
-                </TableCell> */}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  );
-}
 
-export default PromoCodeList;
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(ref, id));
+    setPromoCodeData(promocodeData.filter((row) => row.id !== id));
+  };
+
+    return (
+      <>
+        <TableContainer>
+          <Table aria-label="sticky table" stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Promo Code
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Start Date
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  End Date
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Promo Code Dis.(%)
+                </TableCell>
+                <TableCell align="left" style={{ minWidth: "100px" }}>
+                  Is PromoCode Live
+                </TableCell>
+                <TableCell align="left" style={{ width: "100px" }}>
+                  Action
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {promocodeData.map((row) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                  <TableCell align="left">{String(row.code)}</TableCell>
+                  <TableCell align="left">{String(row.startDate)}</TableCell>
+                  <TableCell align="left">{String(row.endDate)}</TableCell>
+                  <TableCell align="left">
+                    {String(row.discount)}
+                  </TableCell>
+                  <TableCell align="left">
+                    {String(row.discountStatus ? "Yes" : "No")}
+                  </TableCell>
+                  <TableCell align="left">
+                    <Stack spacing={2} direction="row">
+                      <EditIcon
+                        style={{
+                          fontSize: "20px",
+                          color: "blue",
+                          cursor: "pointer",
+                        }}
+                        className="cursor-pointer"
+                      />
+                      <DeleteIcon
+                        style={{
+                          fontSize: "20px",
+                          color: "darkred",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => handleDelete(row.id)}
+                      />
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    );
+  }
+
+  export default PromoCodeList;
