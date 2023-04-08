5688789b4d91f4e90260bf85ca29d396b60761d8
sofiyarao <appsmadeforu@gmail.com>
2023-04-08 11:34:51 +0530

    Add new offer

--- src/offers/components/AddNewOffer.js
+++ src/offers/components/AddNewOffer.js
@@ -6,17 +6,76 @@ import {
   CardHeader,
   CardContent,
   TextField,
+  FormControlLabel,
+  Checkbox,
+  Typography,
+  Button,
 } from "@mui/material";
+import Swal from "sweetalert2";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs from "dayjs";
-function AddNewOffer() {
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { db, storage } from "../../firebase-config";
+import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
+function AddNewOffer({ closeModal, getOfferData }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [startDate, setStartDate] = useState(dayjs(new Date()));
   const [endDate, setEndDate] = useState("");
-  const [discount, setDiscount] = useState(0)
+  const [isOfferLive, setIsOfferLive] = useState(false);
+  const [discount, setDiscount] = useState(0);
+  const [file, setFile] = useState(null);
+  const [percent, setPercent] = useState("")
+  const formatDate = (obj) => {
+    return `${obj.date()}/${obj.month()}/${obj.year()}`;
+  };
+  const saveOffer = async (url) => {
+    console.log(startDate, endDate, dayjs(startDate));
+    const offerRef = collection(db, "Offers");
+    const docData = await addDoc(offerRef, {
+      title,
+      bannerImage: url,
+      description,
+      discountPercent: discount,
+      endDate: formatDate(endDate),
+      startDate: formatDate(startDate),
+      hasBanner: Boolean(url),
+      isOfferLive,
+      products: [],
+    });
+    const id = docData.id;
+    await updateDoc(doc(db, "Offers", id), { saleTag: id });
+    Swal.fire("Submitted!", "New Offer has been added", "success");
+    getOfferData()
+    closeModal();
+  };
+  const submitNewOffer = async () => {
+    const storageRef = ref(storage, `/images/${Math.random() + file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = String(
+          Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100) +
+            "%"
+        );
+        // update progress
+        setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (url) => {
+          await saveOffer(url);
+        });
+      }
+    );
+  };
+  const handleSubmit = () => {
+    if (!title) Swal.fire("Validation Issue!", "Please add a Title", "error");
+    else submitNewOffer();
+  };
   return (
     <Card sx={{ marginTop: "25px", border: "1px solid" }}>
       <CardHeader title="Add New Offer" />
@@ -30,6 +89,19 @@ function AddNewOffer() {
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               label="Title"
+              size="small"
+              sx={{ minWidth: "100%" }}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              type="number"
+              error={false}
+              id="discount"
+              name="discount"
+              value={discount}
+              onChange={(e) => setDiscount(e.target.value)}
+              label="Discount"
               size="small"
               sx={{ minWidth: "100%" }}
             />
@@ -65,22 +137,42 @@ function AddNewOffer() {
             </LocalizationProvider>
           </Grid>
           <Grid item xs={4}>
-            <TextField
-              type="number"
-              error={false}
-              id="discount"
-              name="discount"
-              value={discount}
-              onChange={(e) => setDiscount(e.target.value)}
-              label="Discount"
-              size="small"
-              sx={{ minWidth: "100%", height: "100%" }}
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={isOfferLive}
+                  onChange={(e) => setIsOfferLive(e.target.checked)}
+                  name="isOfferLive"
+                />
+              }
+              name="isOfferLive"
+              sx={{ minWidth: "100%", marginTop: "5px" }}
+              label="Is Offer Live"
             />
+          </Grid>
+          <Grid item>
+            <input
+              type="file"
+              style={{ marginTop: "10px" }}
+              onChange={(e) => setFile(e.target.files[0])}
+              accept="/image/*"
+            />
+          </Grid>
+          {percent && (
+          <Grid item xs={12}>
+            {percent}
+          </Grid>
+        )}
+          <Grid item xs={12}>
+            <Typography variant="h5" align="right">
+              <Button variant="contained" onClick={handleSubmit}>
+                Submit
+              </Button>
+            </Typography>
           </Grid>
         </Grid>
       </CardContent>
     </Card>
-    // </Box>
   );
 }
 
