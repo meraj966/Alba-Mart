ba825e78201a69e63e74b2fe95213f4c3982ad4a
sofiyarao <appsmadeforu@gmail.com>
2023-06-26 23:47:09 +0530

    [UPDATE ] build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+console.log("is this working===================")
 const root = ReactDOM.createRoot(document.getElementById("root"));
 if (root)
   root.render(
