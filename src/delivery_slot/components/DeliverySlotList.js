59963228259e98e478a242498d4d088ab9ca5447
Maaz Haque <merajssm786@gmail.com>
2023-07-15 14:34:58 +0530

    [Removing commented line]

--- src/delivery_slot/components/DeliverySlotList.js
+++ src/delivery_slot/components/DeliverySlotList.js
@@ -1,85 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import {
-//   Paper,
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableContainer,
-//   TableHead,
-//   TableRow,
-//   Stack,
-//   IconButton,
-// } from "@mui/material";
-// import { db } from "../../firebase-config";
-// import { collection, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore";
-// import EditIcon from "@mui/icons-material/Edit";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-// import { Link } from "react-router-dom";
-
-// function DeliverySlotList({ openModal, deliveryslotData, handleDelete }) {
-
-  
-//   return (
-//     <>
-//       <TableContainer>
-//         <Table aria-label="sticky table" stickyHeader>
-//           <TableHead>
-//             <TableRow>
-//               <TableCell align="left" style={{ minWidth: "100px" }}>
-//                 Day
-//               </TableCell>
-//               <TableCell align="left" style={{ minWidth: "100px" }}>
-//                 Slot 1 Time
-//               </TableCell>
-//               <TableCell align="left" style={{ minWidth: "100px" }}>
-//                 No. Of Delvry(Slot 1)
-//               </TableCell>
-//               <TableCell align="left" style={{ width: "100px" }}>
-//                 Action
-//               </TableCell>
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {deliveryslotData?.map((row) => (
-//               <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-//                 <TableCell align="left">{String(row.day)}</TableCell>
-//                 <TableCell align="left">{String(row.slot1Time)}</TableCell>
-//                 <TableCell align="left">{String(row.slot1)}</TableCell>
-//                 <TableCell align="left">
-//                   <Stack spacing={2} direction="row">
-//                     <EditIcon
-//                       style={{
-//                         fontSize: "20px",
-//                         color: "blue",
-//                         cursor: "pointer",
-//                       }}
-//                       className="cursor-pointer"
-//                       onClick={()=> openModal(row)}
-//                     />
-//                     <DeleteIcon
-//                       style={{
-//                         fontSize: "20px",
-//                         color: "darkred",
-//                         cursor: "pointer",
-//                       }}
-//                       onClick={() => handleDelete(row.id)}
-//                     />
-//                   </Stack>
-//                 </TableCell>
-//               </TableRow>
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//     </>
-//   );
-// }
-
-// export default DeliverySlotList;
-
-
-
 import React, { useEffect, useState } from "react";
 import {
   Paper,
@@ -93,30 +11,15 @@ import {
   IconButton,
 } from "@mui/material";
 import { db } from "../../firebase-config";
-import {
-  collection,
-  getDocs,
-  deleteDoc,
-  doc,
-  onSnapshot,
-} from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { Link } from "react-router-dom";
 
 function DeliverySlotList({ openModal, deliveryslotData, handleDelete }) {
-  const [sortedData, setSortedData] = useState([]);
 
-  useEffect(() => {
-    // Sort the deliveryslotData by day in ascending order
-    const sortedData = [...deliveryslotData].sort((a, b) =>
-      a.day.localeCompare(b.day)
-    );
-
-    setSortedData(sortedData);
-  }, [deliveryslotData]);
-
+  
   return (
     <>
       <TableContainer>
@@ -138,7 +41,7 @@ function DeliverySlotList({ openModal, deliveryslotData, handleDelete }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {sortedData.map((row) => (
+            {deliveryslotData?.map((row) => (
               <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                 <TableCell align="left">{String(row.day)}</TableCell>
                 <TableCell align="left">{String(row.slot1Time)}</TableCell>
@@ -152,7 +55,7 @@ function DeliverySlotList({ openModal, deliveryslotData, handleDelete }) {
                         cursor: "pointer",
                       }}
                       className="cursor-pointer"
-                      onClick={() => openModal(row)}
+                      onClick={()=> openModal(row)}
                     />
                     <DeleteIcon
                       style={{
