3f8f31480c858ed74b1371b553248dae877d7314
sofiyarao <appsmadeforu@gmail.com>
2024-07-14 23:32:55 +0530

    [ADD] Lint fix

--- src/authentication/ProtectedRoute.jsx
+++ src/authentication/ProtectedRoute.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import {Navigate, useLocation} from "react-router-dom"
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
-const ProtectedRoute = ({children}) => {
-    const user = window.localStorage.getItem("token");
-    let location = useLocation();
-    console.log("is User logged in =>", user)
-    if(!user) {
-        return <Navigate to="/" state={{ from: location}} replace />
-    }
- return children
-
+const ProtectedRoute = ({ children }) => {
+  const user = window.localStorage.getItem("token");
+  let location = useLocation();
+  console.log("is User logged in =>", user);
+  if (!user) {
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+  return children;
 };
 
 export default ProtectedRoute;
